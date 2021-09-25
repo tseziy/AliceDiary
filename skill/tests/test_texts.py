2c345da775d5ec320cb4ea00948067ba9e151364
Krapivin Andrey <a.krapivin@skbkontur.ru>
2021-09-25 12:24:42 +0500

    feat: Добавил приветственное слово (#9)

---
+++ skill/tests/test_texts.py
@@ -0,0 +1,11 @@
+from fluentcheck import Check
+
+import skill.texts as texts
+
+
+def test_hello_start_session():
+    text, tts = texts.hello()
+    Check(text).is_not_none().is_string()\
+        .matches("^Привет\\! Это цифровой дневник\\.")\
+        .matches(".*Готовы продолжить\\?$")
+
